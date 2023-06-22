f26fab9d230526fa1ae83a6a17d4e1e3611acd7a
Salim izi <tizisalim5@gmail.com>
2023-06-22 18:37:55 +0200

    Add login.html, add some css in it, create routes for login.html

--- LsfAi.Web/src/server.js
+++ LsfAi.Web/src/server.js
@@ -6,9 +6,26 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
+//route for exercise sign reproduction
+app.get('/camera', (req, res) => {
   res.sendFile(path.join(__dirname, './views/frontOffice/exercises/reproduction.html'));
 });
+
+
+// route form
+
+//route for login
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/frontOffice/form/login.html'));
+});
+
+//Route for register
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/frontOffice/form/register.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Express running on port ${port}`);
