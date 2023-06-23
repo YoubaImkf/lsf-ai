0ef941da7af182fb4715f17301d0b4c8e0391e43
Firdaousse <firda@FMOUSSA>
2023-06-23 14:08:14 +0200

    Mobile interface kinda done

--- LsfAi.Web/src/server.js
+++ LsfAi.Web/src/server.js
@@ -6,9 +6,17 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname)));
 
+// ROUTES
+// Reproduction exercise
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './views/frontOffice/exercises/reproduction.html'));
 });
+
+// Dictionary list
+app.get('/dictionary', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/frontOffice/dictionary/dictionary-list.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Express running on port ${port}`);
