a09c9ef60686eef81262c5210dbc4f62cb207be5
youba.imakhlaf <mevshypocrisie@yahoo.com>
2023-06-23 17:25:08 +0200

    🦺 (form): add input control into script files

--- LsfAi.Web/src/scripts/loginScript.js
+++ LsfAi.Web/src/scripts/loginScript.js
@@ -1,4 +1,6 @@
 const form = document.getElementById("login-form");
+const emailError = document.getElementById("email-error");
+const passwordError = document.getElementById("password-error");
 
 function postData(data) {
   fetch("http://localhost:8282/users/login", {
@@ -23,12 +25,43 @@ function postData(data) {
     });
 }
 
+function validateForm() {
+  // Reset error messages and input styles
+  emailError.textContent = "";
+  passwordError.textContent = "";
+  form.elements.email.classList.remove("error-input");
+  form.elements.password.classList.remove("error-input");
+
+  // Get form values
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  // Check if email and password are filled
+  if (!email) {
+    emailError.textContent = "Email is required";
+    form.elements.email.classList.add("error-input");
+    return false;
+  }
+  if (!password) {
+    passwordError.textContent = "Password is required";
+    form.elements.password.classList.add("error-input");
+    return false;
+  }
+
+  return true;
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   // Get form values
   const email = form.elements.email.value;
   const password = form.elements.password.value;
   console.log(email, password);
+
+  if (!validateForm()) {
+    return;
+  }
+
 
   let data = {
     email: email,
--- LsfAi.Web/src/scripts/registerScript.js
+++ LsfAi.Web/src/scripts/registerScript.js
@@ -1,4 +1,7 @@
 const form = document.getElementById("registration-form");
+const emailError = document.getElementById("email-error");
+const usernameError = document.getElementById("username-error");
+const passwordError = document.getElementById("password-error");
 
 function postData(data) {
   fetch("http://localhost:8282/users/register", {
@@ -23,12 +26,58 @@ function postData(data) {
     });
 }
 
+function validateForm() {
+  // Reset error messages and input styles
+  emailError.textContent = "";
+  usernameError.textContent = "";
+  passwordError.textContent = "";
+  form.elements.email.classList.remove("error-input");
+  form.elements.username.classList.remove("error-input");
+  form.elements.password.classList.remove("error-input");;
+
+  // Get form values
+  const email = form.elements.email.value;
+  const username = form.elements.username.value;
+  const password = form.elements.password.value;
+
+  // Check if email, username, and password are filled
+  if (!email) {
+    emailError.textContent = "Email is required";
+    form.elements.email.classList.add("error-input");
+    return false;
+  }
+  if (!username) {
+    usernameError.textContent = "Username is required";
+    form.elements.username.classList.add("error-input");
+    return false;
+  }
+  if (!password) {
+    passwordError.textContent = "Password is required";
+    form.elements.password.classList.add("error-input");
+    return false;
+  }
+
+  // Check password length
+  if (password.length < 8) {
+    passwordError.textContent = "Password must be at least 8 characters long";
+    form.elements.password.classList.add("error-input");
+    return false;
+  }
+
+  return true;
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   // Get form values
   const email = form.elements.email.value;
   const username = form.elements.username.value;
   const password = form.elements.password.value;
+
+  // Validate form inputs
+  if (!validateForm()) {
+    return;
+  }
 
   let data = {
     email: email,
