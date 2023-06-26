55c02065894c52bdda769832d27aa504be665a7e
Salim izi <tizisalim5@gmail.com>
2023-06-26 18:17:15 +0200

    update training view, add a popup when user succeed to reproduce a signe

--- LsfAi.Web/src/services/WebcamService.js
+++ LsfAi.Web/src/services/WebcamService.js
@@ -5,7 +5,6 @@ class WebCamComponent {
       this.model = null;
       this.webcam = null;
       this.labelContainer = null;
-      this.popupContainer = null;
       this.maxPredictions = null;
       this.isIos = false;
     }
@@ -29,7 +28,6 @@ class WebCamComponent {
       this.labelContainer.id = 'label-container';
       document.getElementById(this.containerId).appendChild(this.webcam.canvas);
       document.getElementById(this.containerId).appendChild(this.labelContainer);
-      document.getElementById(this.containerId).appendChild(this.popupContainer);
     }
   
     async loop() {
@@ -46,35 +44,56 @@ class WebCamComponent {
         prediction = await this.model.predict(this.webcam.canvas);
       }
 
+      //Pop up to show if the prediction is greater than 0.8
+      let popup = document.querySelector('.popup-container-show');
+
+      // if (!labelChild) {
+      //   labelChild = document.createElement('div');
+      //   labelChild.id = `prediction-0`;
+      //   this.labelContainer.appendChild(labelChild);
+      // }
+
+      if(prediction[1].probability.toFixed(2) > 0.8){
+        popup.style.display = "block";
+        // labelChild.style.color = "green";
+      } 
+      // else {
+      //   labelChild.style.color = "red";
+      // }
+
+      // const classPrediction = prediction[1].className + ': ' + prediction[1].probability.toFixed(2);
+      // this.labelContainer.childNodes[0].innerHTML = classPrediction;
+
+
       
 
-        for(let i = 0; i < this.maxPredictions; i++){
+        // for(let i = 0; i < this.maxPredictions; i++){
 
-          let labelChild =  this.labelContainer.querySelector(`#prediction-${i}`);
-          let popup = this.popupContainer.querySelector('.popup-container');
-          // Check if the child already exists before creating it
-          if (!labelChild) {
-            labelChild = document.createElement('div');
-            labelChild.id = `prediction-${i}`;
-            this.labelContainer.appendChild(labelChild);
-          }
+        //   let labelChild =  this.labelContainer.querySelector(`#prediction-${i}`);
+        //   // let popup = this.popupContainer.querySelector('.popup-container');
+        //   // Check if the child already exists before creating it
+        //   if (!labelChild) {
+        //     labelChild = document.createElement('div');
+        //     labelChild.id = `prediction-${i}`;
+        //     this.labelContainer.appendChild(labelChild);
+        //   }
 
-          // Put green color if probability is greater than 0.6
+        //   // Put green color if probability is greater than 0.6
 
-          if(prediction[i].probability.toFixed(2) > 0.6){
-            popup = document.createElement('div');
-            popup.className = 'popup-container';
-            this.popupContainer.appendChild(popup);
-            labelChild.style.color = "green";
-          } else {
-            labelChild.style.color = "red";
-          }
+        //   if(prediction[i].probability.toFixed(2) > 0.8){
+        //     // popup = document.createElement('div');
+        //     // popup.className = 'popup-container';
+        //     // this.popupContainer.appendChild(popup);
+        //     labelChild.style.color = "green";
+        //   } else {
+        //     labelChild.style.color = "red";
+        //   }
 
-          // Put the prediction text in the label
-          const classPrediction = prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
-          this.labelContainer.childNodes[i].innerHTML = classPrediction;
+        //   // Put the prediction text in the label
+        //   const classPrediction = prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+        //   this.labelContainer.childNodes[i].innerHTML = classPrediction;
 
-        }
+        // }
     }
   }
   
