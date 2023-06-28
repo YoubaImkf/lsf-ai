01c1541fe8c13808d4655a78c9dff9f043d73147
Salim izi <tizisalim5@gmail.com>
2023-06-28 19:06:51 +0200

    register button disapear when user is connected

--- LsfAi.Web/src/scripts/authentication/updateAccountScript.js
+++ LsfAi.Web/src/scripts/authentication/updateAccountScript.js
@@ -1,5 +1,7 @@
 const form = document.getElementById("account-form");
 
+const passwordError = document.getElementById("password-error");
+
 // Get user from session storage
 const user = JSON.parse(sessionStorage.getItem('user'));
 
@@ -49,17 +51,48 @@ async function modifyUser(data) {
       setTimeout(clearMessages, 2000);
     } else {
       console.error('Failed to update user:', response.status);
-      errorMessage.textContent = 'Une erreur est survenue lors de la modification';
-      errorMessage.classList.add('error-message');
-      setTimeout(clearMessages, 2000);
     }
   } catch (error) {
     console.error('Failed to update user:', error);
   }
 }
 
+async function validateForm() {
+  // Reset error messages and input styles
+  passwordError.textContent = "";
+
+  form.elements.password.classList.remove("error-input");
+
+  // Get form values
+  const password = form.elements.password.value;
+
+  // Check if password are filled
+  if (!password) {
+    passwordError.textContent = "Mot de passe requis";
+    form.elements.password.classList.add("error-input");
+    return false;
+  }
+
+  // Check if password is at least 8 characters long
+  if(password.length < 8){
+    passwordError.textContent = "Le mot de passe doit contenir au moins 8 caractères";
+    form.elements.password.classList.add("error-input");
+    return false;
+  }
+
+  //check password complexity
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!passwordRegex.test(password)) {
+    passwordError.textContent =
+      "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial";
+    form.elements.password.classList.add("error-input");
+    return false;
+  }
+}
+
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
+    if(await validateForm()){
       // Get form values
       const email = user.email;
       const password = form.elements.password.value;
@@ -74,6 +107,7 @@ form.addEventListener("submit", async (event) => {
       console.log("data" + JSON.stringify(data));
   
       modifyUser(data);
+    }
   });
 
 
